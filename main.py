67f4edfc18cd7ebd85aed6c2b2f038ea44c41ca7
andjf <ferrinaj@vt.edu>
2021-12-08 00:45:21 -0500

    add verbose graphical terminal output

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #! usr/bin/env python3
 
+import colorama
 from Deck import Deck
 from Card import Card
 from Hand import Hand
 from itertools import combinations as C
 from time import time
+from colorama import Fore
+
+
+def pos(x, y):
+    # Sets the position of the cursor in the terminal
+    return f"\x1b[{y};{x}H"
 
 
 def list_without(i):
@@ -12,41 +19,56 @@ def list_without(i):
     # [0-51] without the number i
     return list(range(0, i)) + list(range(i + 1, 52))
 
-def get_frequencies():
-    
+
+def get_frequencies(verbose=True):
+
+    # Clear the screen and move to (0, 0)
+    if verbose:
+        print("\033[2J")
+        print(Fore.GREEN + f"{pos(0, 0)}Counting All Possible Cribbage Hands:" + Fore.RESET)
+
     # Get the start time of the loop
     start_time = time()
 
-    # A list where the value at each index is the 
+    # A list where the value at each index is the
     # number of hands that produce that score
     scores = [0] * 30
-    
+
     # Get deck that cards will be taken from
     deck = Deck()
-    
+
+    # Set the loading bar width
+    bar_width = 30
+
     # Loop through each possible draw card
     # use enumerate to also get the index
     for draw_index, draw in enumerate(deck.cards):
 
+        # Inform the user on the current status of the loop
+        if verbose:
+            print(Fore.YELLOW + f"{pos(0, 2)}Currently Calculating: {draw} ({draw_index + 1}/52)" + Fore.RESET)
+
         # Loop through each possible combination of the deck
         # such that the hand comes from the deck that doesn't
-        # contain the current draw card (choose 4 cards)
-        for indices in C(list_without(draw_index), 4):
+        # contain the current draw card (choose 4 cards) (C(51, 4) = 249,900)
+        for hand_index, indices in enumerate(C(list_without(draw_index), 4)):
+
+            # Inform user about progress of current scoring with loading bar
+            if verbose and hand_index % (249900 // bar_width) == 0:
+                curr_bar_width = int((hand_index / 249900) * bar_width)
+                print(Fore.LIGHTYELLOW_EX + f"{pos(0, 3)}|{'#'*curr_bar_width}{' '*(bar_width-curr_bar_width - 1)}|" + Fore.RESET)
 
             # Get the current cards that should be a part of the hand
             curr_cards = [deck.cards[i] for i in indices]
 
             # Add one to the scores list at the index of the score of the hand
             scores[Hand(curr_cards, draw).get_score()] += 1
-        
-        # Notify the user that a specific draw card has been explored
-        print(f"Done with {draw}")
 
-    # Print the scores frequency list
-    print(scores)
-
-    # Print the amount of time that it took to explore
-    print(f"Took {time()-start_time:.02f} seconds")
+    if verbose:
+        # Print the scores frequency list
+        print(scores)
+        # Print the amount of time that it took to explore
+        print(f"Took {time()-start_time:.02f} seconds")
 
     # return the scores
     return scores
@@ -62,6 +84,7 @@ def main():
             print(f"╚════╣ Should be {t:<8} -  found {m:<8}\n")
     if not mismatch:
         print("Program accurately reproduces results")
+
 
 if __name__ == "__main__":
     main()
